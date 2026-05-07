fe683c0111f63e5497f822d2da69254e3aa219f5
DeanMave <dean.tarantul@gmail.com>
2026-05-07 16:41:13 +0300

    feat: добавлен доменный класс Column и enum ColumnStatus

---
+++ src/main/java/io/github/deanmave/hplclims/domain/Column.java
@@ -0,0 +1,24 @@
+package io.github.deanmave.hplclims.domain;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Column {
+    private Long id;
+    private String manufacturer;
+    private String serialNumber;
+    private String partNumber;
+    private Integer length;
+    private Integer diameter;
+    private Double particleSize;
+    private LocalDate installationDate;
+    private Integer phMin;
+    private Integer phMax;
+    private String stationaryPhase;
+    private Integer maxPressure;
+    private String ownerOrganization;
+    private LocalDate returnDate;
+    private ColumnStatus status;
+}
