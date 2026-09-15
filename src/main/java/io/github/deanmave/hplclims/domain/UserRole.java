ff3c00c6714acaea65a229e955ebe801fd303fcd
DeanMave <dean.tarantul@gmail.com>
2026-09-15 15:36:49 +0300

    Убрать роль Registrar в UserRole.java

--- src/main/java/io/github/deanmave/hplclims/domain/UserRole.java
+++ src/main/java/io/github/deanmave/hplclims/domain/UserRole.java
@@ -3,6 +3,7 @@ package io.github.deanmave.hplclims.domain;
 public enum UserRole {
     SYSTEM_ADMIN,
     ADMIN,
+    //TO DO: роли REGISTRAR больше не существует
     REGISTRAR,
     USER,
     VIEWER
