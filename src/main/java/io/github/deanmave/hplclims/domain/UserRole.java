330a77372112032cab5317e8cd6edd5185a6ebba
DeanMave <dean.tarantul@gmail.com>
2026-09-16 09:14:03 +0300

    fix: убрана роль REGISTRAR в UserRole.java

--- src/main/java/io/github/deanmave/hplclims/domain/UserRole.java
+++ src/main/java/io/github/deanmave/hplclims/domain/UserRole.java
@@ -3,8 +3,6 @@ package io.github.deanmave.hplclims.domain;
 public enum UserRole {
     SYSTEM_ADMIN,
     ADMIN,
-    //TO DO: роли REGISTRAR больше не существует
-    REGISTRAR,
     USER,
     VIEWER
 }
