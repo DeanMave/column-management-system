e05322f00b704ffd6af5cd6154e855ecd81cabd3
DeanMave <dean.tarantul@gmail.com>
2026-09-16 13:25:04 +0300

    feat: добавлен метод для исправления существующего лога UsageLogService.java

--- src/main/java/io/github/deanmave/hplclims/service/interfaces/UsageLogService.java
+++ src/main/java/io/github/deanmave/hplclims/service/interfaces/UsageLogService.java
@@ -3,6 +3,7 @@ package io.github.deanmave.hplclims.service.interfaces;
 import io.github.deanmave.hplclims.domain.ColumnUsageLog;
 import io.github.deanmave.hplclims.domain.dto.request.EndUsageRequest;
 import io.github.deanmave.hplclims.domain.dto.request.StartUsageRequest;
+import io.github.deanmave.hplclims.domain.dto.request.CorrectUsageLogRequest;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,6 +14,8 @@ public interface UsageLogService {
     ColumnUsageLog endUsage(Long logId, EndUsageRequest request);
 
     ColumnUsageLog rejectUsage(Long logId, String reason, LocalDate rejectionDate);
+
+    ColumnUsageLog correctLog(Long logId, CorrectUsageRequest request);
 
     List<ColumnUsageLog> getLogsByColumn(Long hplcColumnId);
 
